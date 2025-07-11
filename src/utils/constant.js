a88cd52869f33b42d1b9787e8321a324e5e3f508
PonDevCode <pondevcode@gmail.com>
2025-07-11 16:31:00 +0700

    update api local and production

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,8 +1,8 @@
-let API = ''
+let API = 'https://ba-taska.onrender.com'
 if (import.meta.env.VITE_BUILD_MODE === 'dev') {
     API = 'http://localhost:8000'
 } else {
     API = 'https://ba-taska.onrender.com'
 }
-console.log("🚀 ~ API:", API)
+console.log("🚀 ~ API:", import.meta.env.VITE_BUILD_MODE)
 export const API_ROOT = API
